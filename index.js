a8a7e9996e2b3d514de316612bb99bc971ecc6e6
Toshmoney <abdulmaleeqomotosho@gmail.com>
2024-08-03 11:26:40 +0100

    cors updated

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ const upload = multer({ dest: 'uploads/' })
 
 app.use(express.json());
 app.use(fileUpload());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
 app.use(cors({origin:"*"}))
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
